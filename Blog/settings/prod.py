2a002d5c7af8d69020b882509d79856b44eca9f3
Mahmoud <hotagx3@gmail.com>
2024-07-15 22:10:58 +0400

    deployment

--- Blog/settings/prod.py
+++ Blog/settings/prod.py
@@ -1,7 +1,18 @@
 import os
+import dj_database_url
 from .common import *
 
 
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-prod-0c3f86c27938.herokuapp.com']
+
+
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+DATABASES['default']['OPTIONS'] = {
+    'charset': 'utf8mb4',
+}
