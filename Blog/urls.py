3289388a52ff277875aefe976f5e4a9af7afad20
Mahmoud <hotagx3@gmail.com>
2024-07-16 14:49:25 +0400

    Update S3 settings for public read access

--- Blog/urls.py
+++ Blog/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from Blog.settings import common
@@ -24,7 +25,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('weblog.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-] + static(common.MEDIA_URL, document_root=common.MEDIA_ROOT)
+]
 
+if settings.DEBUG:
+    # Serve media files from MEDIA_ROOT in development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# Always include Silk URLs (you might want to restrict this in production)
 urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
